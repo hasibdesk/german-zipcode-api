53e67d7324b8f93aeea47e8cc30951e314958728
Md Hasibul Hossain <hasibdesk@gmail.com>
2022-02-09 02:33:50 +0600

    Unique zipcode filter

--- routes/zipcode.js
+++ routes/zipcode.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get('/zipcodes', (req, res) => {
-	var zipcodes = JSON.parse(fs.readFileSync('zipcode.json', 'utf8'));
-	res.status(200).json(zipcodes);
+	var zipcodes = JSON.parse(fs.readFileSync('postcode.json', 'utf8'));
+	let uniqueZipcodes = [...new Map(zipcodes.map((item) => [item['zipcode'], item])).values()];
+	console.log(uniqueZipcodes.length);
+	res.status(200).json(uniqueZipcodes);
 });
 
 module.exports = router;
